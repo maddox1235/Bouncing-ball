df3297e6f76d536c38a1db7eafae793121b2c6ce
gamermaddox123 <gamermaddox123@gmail.com>
2021-04-22 08:50:47 +0200

    created repository

---
+++ main.py
@@ -0,0 +1,37 @@
+
+# bouncing ball simulator
+# by @maddox
+# part 1: getting started
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("black")
+wn.title("Bouncing ball simulator")
+
+ball = turtle.Turtle()
+ball.shape("circle")
+ball.color("green")
+ball.penup()
+ball.speed(0)
+ball.goto(0,200)
+ball.dy = 0
+ball.dx = 2
+
+gravity = 0.1
+
+while True:
+
+ ball.dy -= gravity
+ ball.sety(ball.ycor() + ball.dy)
+
+ball.setx(ball.xcor() + ball.dx)
+
+         # check for a wall collision
+         if ball.xcor() > 300:
+             ball.dx *= -1
+
+             if ball.xcor() < -300:
+                 ball.dx *= -1
+
+           # check for a bounce
+          if ball.ycor() < -300:
+        ball.dy *= -1
